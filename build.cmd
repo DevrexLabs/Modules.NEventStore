1e2d6ab446cab9217b67e523b425a49faaa7ab60
rofr <robert@devrex.se>
2014-07-07 21:53:02 +0200

    initial commit

---
+++ build.cmd
@@ -0,0 +1,18 @@
+@echo off
+ 
+:Build
+cls
+  
+if not exist tools\Cake\Cake.exe ( 
+  	echo Installing Cake...
+ 	nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -NonInteractive
+	 	echo.
+)
+			 
+SET BUILDMODE="Release"
+IF NOT [%1]==[] (set BUILDMODE="%1")
+			  
+echo Starting Cake...
+"tools\Cake\Cake.exe" "build.csx" "-verbosity=verbose" "-config=%BUILDMODE%"
+			   
+exit /b %errorlevel%
